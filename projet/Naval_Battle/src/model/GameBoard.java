fd19cc06c883d8ba7965b4c64294b4829a7368db
bastiangardel <bastian.gardel@heig-vd.ch>
2014-05-20 11:59:40 +0200

    gameboard evolution

--- projet/Naval_Battle/src/model/GameBoard.java
+++ projet/Naval_Battle/src/model/GameBoard.java
@@ -1,5 +1,33 @@
 package model;
 
 public class GameBoard {
+	MapSize mapDefinition;
+	PlayField[][] playfield;
+	
+	
+	
+}
 
+class PlayField {
+	boolean occupied;
+	boolean hit;
+	
+	public boolean isOccupied() {
+		return occupied;
+	}
+	public void setOccupied(boolean occupied) {
+		this.occupied = occupied;
+	}
+	public boolean isHit() {
+		return hit;
+	}
+	public void setHit(boolean hit) {
+		this.hit = hit;
+	}
+	public PlayField(boolean occupied, boolean hit) {
+		super();
+		this.occupied = occupied;
+		this.hit = hit;
+	}
+	
 }
